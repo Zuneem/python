84dc66c029c89db83e148c6b9910e11e265f7979
Zuneem <88955869+Zuneem@users.noreply.github.com>
2021-08-17 21:43:38 -0500

    Add files via upload

---
+++ drinking age.py
@@ -0,0 +1,13 @@
+print ('Hello, how old are you?')
+age = int(input())
+
+if age >= 21:
+  print ('you are old enough to drink!!')
+
+elif age == 20:
+    print ('you will be ' + str(age+1) + ' soon. come back next year.')
+
+else:
+    print ('you are not old enough')
+
+
