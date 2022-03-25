1829d1d01b1256e68ebe353d1755d657ed016eb8
Zuneem <88955869+Zuneem@users.noreply.github.com>
2022-03-24 22:14:15 -0500

    Add files via upload

---
+++ defining_functions.py
@@ -0,0 +1,3 @@
+def time_seconds ( hours, minutes, seconds):
+    print((hours * 3600) + (minutes * 60) + (seconds))
+time_seconds(14,2,2)
---
+++ returning_values.py
@@ -0,0 +1,9 @@
+def area_rectangle(length, width):
+    return (length * width)
+
+area1 = area_rectangle(14, 12)
+area2 = area_rectangle(16,14)
+area3 = area_rectangle(12, 16)
+total_area = area1 + area2 + area3
+
+print(" total area of all rectangles is: " + str(total_area))
